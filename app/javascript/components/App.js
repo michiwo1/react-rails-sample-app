3277fb6a84928fcd0bc2f491e4a254ef8d0c1f59
michiwo <mk1104kado@gmail.com>
2021-04-28 17:35:13 +0900

    編集画面を作成

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import "./App.css";
 import { TodoList } from "./TodoList";
 import { AddTodo } from "./AddTodo";
-import { EditTodo } from "./EditTodo";
+import EditTodo from "./EditTodo";
 
 const Nabvar = styled.nav`
   background: #dbfffe;
