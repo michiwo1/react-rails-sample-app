549d6da60e23bc5bf4359e143dd49f0cb8e0c00c
michiwo1 <mk1104kado@gmail.com>
2021-04-29 21:37:18 +0900

    コンポーネント分割

---
+++ app/javascript/components/Nabver.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nabvar = styled.nav`
+  background: #dbfffe;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`;
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`;
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const Nabver = () => {
+  return (
+    <Nabvar>
+      <Logo>
+        <Link to="/todos">TODO</Link>
+      </Logo>
+      <NavItems>
+        <NavItem>
+          <Link to="/todos">Todosリスト</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/todos/new">新しいTodoを追加</Link>
+        </NavItem>
+      </NavItems>
+    </Nabvar>
+  );
+};
